85f553fb119900c7972911ef02cd494e2743634b
yuxinliuhubert <yuxinliuhubert@gmail.com>
2023-06-12 11:59:10 -0700

    update

--- sketch_jun10a/sketch_jun10a.ino
+++ sketch_jun10a/sketch_jun10a.ino
@@ -1,9 +1,11 @@
 #define INTERRUPT_PIN 4
 #define PRESSURE_PIN 36
-
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h> 
 volatile unsigned long interruptCounter = 0;
 unsigned long lastResetTime = 0;
 volatile unsigned long lastInterruptTime = 0;
+ LiquidCrystal_I2C lcd(0x27,16,2);  // Change the 0x27  i2c address to your i2c address
 
 void IRAM_ATTR handleInterrupt() {
   interruptCounter++;
@@ -16,6 +18,18 @@ void setup() {
   pinMode(PRESSURE_PIN, INPUT_PULLDOWN);
   
   Serial.begin(115200);
+  
+  // lcd.begin(16,2);
+  // lcd.begin(16, 2);
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+  lcd.setCursor(1,0);
+  lcd.print("Hello, world!");
+  lcd.setCursor(3,1);
+  lcd.print("Air Supply");
+  delay(1000);
+  lcd.clear();
 }
 
 void loop() {
@@ -32,7 +46,21 @@ void loop() {
     unsigned long timeSinceLastPulse = currentTime - lastInterruptTime;
     interrupts();
 
+     // Read and print the pressure transducer value
+    int pressureValue = analogRead(PRESSURE_PIN);
+    Serial.print("Pressure transducer value: ");
+    Serial.println(pressureValue);
+
       Serial.println(count);
+      // lcd.clear();
+      lcd.setCursor(2,0);
+      lcd.print("flow count:");
+      lcd.print(count);
+      lcd.print("          ");
+      lcd.setCursor(2,1);
+      lcd.print("Pressure: ");
+      lcd.print(pressureValue);
+      lcd.print("          ");
 
     // // Check if a pulse has been received in the last second
     // if (timeSinceLastPulse < 1000) {
@@ -42,11 +70,7 @@ void loop() {
     //   Serial.println("No flow detected in the last second.");
     // }
 
-    // Read and print the pressure transducer value
-    int pressureValue = analogRead(PRESSURE_PIN);
-    Serial.print("Pressure transducer value: ");
-    Serial.println(pressureValue);
-    delay(30);
+    // delay(30);
   }
 }
 
