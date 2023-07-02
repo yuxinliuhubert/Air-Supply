a97e18d22b3bbfff3150fcfe7bce3e839120d47c
yuxinliuhubert <yuxinliuhubert@gmail.com>
2023-07-02 17:11:12 -0400

    Update sketch_jun10a.ino

--- sketch_jun10a/sketch_jun10a.ino
+++ sketch_jun10a/sketch_jun10a.ino
@@ -1,14 +1,13 @@
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h> 
-#include <Wire.h>
 #include <Adafruit_INA219.h>
 
 Adafruit_INA219 ina219;
 
-#define INTERRUPT_PIN 4
-#define PRESSURE_PIN 36
+#define INTERRUPT_PIN 14
+#define PRESSURE_PIN 27
 
-#define PWM_PIN 15
+#define PWM_PIN 23
 #define PWM_FREQ 5000  // Frequency for PWM signal
 #define PWM_RESOLUTION 8  // Resolution for PWM signal in bits
 #define PWM_CHANNEL 0  // PWM channel
@@ -18,9 +17,9 @@ volatile unsigned long interruptCounter = 0;
 unsigned long lastResetTime = 0;
 volatile unsigned long lastInterruptTime = 0;
  LiquidCrystal_I2C lcd(0x27,16,2);  // Change the 0x27  i2c address to your i2c address
-
+LiquidCrystal_I2C lcd2(0x26,16,2);
  // Declare global variables for duty cycle
-int dutyCycle[] = {0, 178,178,255,255};  // Corresponds to 0%, 30%, 60%, 80% and 100% duty cycle in 8-bit resolution
+int dutyCycle[] = {0,178};  // Corresponds to 0%, 30%, 60%, 80% and 100% duty cycle in 8-bit resolution
 int dutyIndex = 0;  // Initialize index for dutyCycle array
 
 void IRAM_ATTR handleInterrupt() {
@@ -37,25 +36,42 @@ void setup() {
     Serial.println("Failed to find INA219 chip");
     while (1) { delay(10); }
   }
-  
+  Wire.begin(21,22);
   Serial.begin(115200);
 
   // Set up PWM on PWM_PIN
   pinMode(PWM_PIN, OUTPUT);
   ledcSetup(PWM_CHANNEL, PWM_FREQ, PWM_RESOLUTION);
   ledcAttachPin(PWM_PIN, PWM_CHANNEL);
+  ledcWrite(PWM_CHANNEL, 0);
   
   // lcd.begin(16,2);
   // lcd.begin(16, 2);
   lcd.init();
   lcd.backlight();
+
+  lcd2.init();
+  lcd2.backlight();
+
+
   lcd.clear();
+  lcd2.clear();
+
   lcd.setCursor(1,0);
-  lcd.print("Hello, world!");
-  lcd.setCursor(3,1);
+  // lcd.print("Hello, world!");
   lcd.print("Air Supply");
+  lcd.setCursor(3,1);
+  lcd.print("Screen 1");
+
+  lcd2.setCursor(1,0);
+  lcd2.print("Air Supply");
+  lcd2.setCursor(3,1);
+  lcd2.print("Screen 2");
   delay(1000);
+
   lcd.clear();
+  lcd2.clear();
+
 
 }
 
@@ -78,32 +94,42 @@ void loop() {
     Serial.print("Pressure transducer value: ");
     Serial.println(pressureValue);
 
+    Serial.print("flow rate:");
       Serial.println(count);
       // lcd.clear();
       lcd.setCursor(0,0);
-      lcd.print("flow:");
+      lcd.print("flow rate: ");
       lcd.print(count);
       lcd.print("       ");
 
-      float current_mA = ina219.getCurrent_mA();
-    Serial.print("Current: "); Serial.print(current_mA); Serial.println(" mA");
-    lcd.setCursor(9,0);
-    // lcd.print("Current: ");
-    lcd.print(current_mA);
-    lcd.print("mA");
-    lcd.print("          ");
-
-
-      lcd.setCursor(1,1);
+      lcd.setCursor(0,1);
       lcd.print("Pressure: ");
       lcd.print(pressureValue);
       lcd.print("          ");
 
+      float current_mA = ina219.getCurrent_mA();
+    Serial.print("Current: "); Serial.print(current_mA); Serial.println(" mA"); 
+    lcd2.setCursor(0,0);
+    lcd2.print("I: ");
+    lcd2.print(current_mA);
+    lcd2.print(" mA");
+    lcd2.print("          ");
+
+    lcd2.setCursor(0,1);
+    lcd2.print("Use Serial Monitor");
+    lcd2.print(current_mA);
+    lcd2.print("mA");
+    lcd2.print("          ");
+
+    Serial.println("Type in number between 0 (close) and 255 (open) to control valve!");
+    Serial.println();
+
+
      
       // Change duty cycle of PWM signal
-  ledcWrite(PWM_CHANNEL, dutyCycle[dutyIndex]);
+  // ledcWrite(PWM_CHANNEL, dutyCycle[dutyIndex]);
 
-  dutyIndex = (dutyIndex + 1) % 5;  // Cycle through duty cycle values
+  // dutyIndex = (dutyIndex + 1) % 2;  // Cycle through duty cycle values
 
   // delay(1000);  // Wait for 1 second before changing duty cycle
 
@@ -116,6 +142,21 @@ void loop() {
     // }
 
     // delay(30);
+  } else {
+      if(Serial.available() > 0) {
+      String inputString = Serial.readStringUntil('\n');
+      inputString.trim();
+      int inputInt = inputString.toInt();
+
+      if(inputInt >= 0 && inputInt <= 255) {
+        ledcWrite(PWM_CHANNEL, inputInt);
+        Serial.print("Updated duty cycle: ");
+        Serial.println(inputInt);
+      }
+      else {
+        Serial.println("Invalid input! Please enter a number between 0 and 255.");
+      }
+      }
   }
 }
 
