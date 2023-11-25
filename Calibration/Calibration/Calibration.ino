4425a40a34c45d2d2add2dfce1d2df41206177ec
yuxinliuhubert <52901645+yuxinliuhubert@users.noreply.github.com>
2023-11-24 21:36:58 -0800

    Update

--- Calibration/Calibration/Calibration.ino
+++ Calibration/Calibration/Calibration.ino
@@ -1,18 +1,21 @@
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 #include <Adafruit_INA219.h>
+#include "esp_timer.h"
+
 
 // 0 if PT, 1 if flow sensor.
-volatile int calibrationCase = 0;
+volatile int calibrationCase = 1;
 
 // MODIFY HERE ACCORDING TO CALIBRATION VALUES
 // reset them to a = 1, b = 0 if recalibrate!
-volatile float PT_a = 1;
-volatile float PT_b = 0;
+volatile float PT_a = 0.0232;
+volatile float PT_b = -10.9232;
 volatile float FL_a = 1;
 volatile float FL_b = 0;
 
 
+volatile bool updateLCD = false;
 volatile int timerInterval = 10;
 Adafruit_INA219 ina219;
 #define INTERRUPT_PIN 14
@@ -27,12 +30,23 @@ unsigned long lastResetTime = 0;
 volatile unsigned long lastInterruptTime = 0;
 LiquidCrystal_I2C lcd(0x27, 16, 2);  // Change the 0x27  i2c address to your i2c address
 LiquidCrystal_I2C lcd2(0x26, 16, 2);
-
+portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
 
 void IRAM_ATTR handleInterrupt() {
   interruptCounter++;
   // lastInterruptTime = millis();  // Record the time of the last pulse
 }
+
+
+hw_timer_t * timer = NULL;
+void IRAM_ATTR onTimer() {
+  portENTER_CRITICAL_ISR(&timerMux);
+  updateLCD = true;
+  portEXIT_CRITICAL_ISR(&timerMux);
+}
+
+
+
 
 void lcdPrint(float current_mA, float pressureValue, float flowRate) {
   lcd.setCursor(0, 0);
@@ -61,12 +75,16 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), handleInterrupt, RISING);
   pinMode(PRESSURE_PIN, INPUT_PULLDOWN);
 
+
   if (!ina219.begin()) {
     Serial.println("Failed to find INA219 chip");
     // while (1) { delay(10); }
   }
   Wire.begin(21, 22);
   Serial.begin(115200);
+  timer = timerBegin(0, 80, true);
+  timerAttachInterrupt(timer, &onTimer, true);
+timerAlarmWrite(timer, 1000000, true);
 
 
   // Set up PWM on PWM_PIN
@@ -75,57 +93,55 @@ void setup() {
   ledcAttachPin(PWM_PIN, PWM_CHANNEL);
   ledcWrite(PWM_CHANNEL, 255);
 
-  if (calibrationCase == 1) {
-    timerInterval = 1000;
-
-  }
-
-  // // lcd.begin(16,2);
-  // // lcd.begin(16, 2);
-  // lcd.init();
-  // lcd.backlight();
-
-  // lcd2.init();
-  // lcd2.backlight();
 
 
-  // lcd.clear();
-  // lcd2.clear();
+  // lcd.begin(16,2);
+  // lcd.begin(16, 2);
+  lcd.init();
+  lcd.backlight();
 
-  // lcd.setCursor(1, 0);
-  // // lcd.print("Hello, world!");
-  // lcd.print("Air Supply");
-  // lcd.setCursor(3, 1);
-  // lcd.print("Screen 1");
+  lcd2.init();
+  lcd2.backlight();
 
-  // lcd2.setCursor(1, 0);
-  // lcd2.print("Air Supply");
-  // lcd2.setCursor(3, 1);
-  // lcd2.print("Screen 2");
-  // delay(1000);
 
-  // lcd.clear();
-  // lcd2.clear();
+  lcd.clear();
+  lcd2.clear();
+
+  lcd.setCursor(1, 0);
+  // lcd.print("Hello, world!");
+  lcd.print("Air Supply");
+  lcd.setCursor(3, 1);
+  lcd.print("Screen 1");
+
+  lcd2.setCursor(1, 0);
+  lcd2.print("Air Supply");
+  lcd2.setCursor(3, 1);
+  lcd2.print("Screen 2");
+  delay(1000);
+
+  lcd.clear();
+  lcd2.clear();
+
+  timerAlarmEnable(timer);
 }
 
 void loop() {
   unsigned long currentTime = millis();
+  float current_mA = ina219.getCurrent_mA();
+
 
   // If 1 second has passed since the last reset
   if (currentTime - lastResetTime >= timerInterval) {
     lastResetTime = currentTime;
 
-    if (calibrationCase == 0) {
-      timerInterval = 10;
+      // timerInterval = 10;
          // Read and print the pressure transducer value
     float pressureValue = analogRead(PRESSURE_PIN) * PT_a + PT_b;
     // Read and calculate flow rate
- 
 
-      Serial.println(pressureValue);
-    } else if (calibrationCase == 1) {
-      timerInterval = 1000;
+      // Serial.println(pressureValue);
 
+    if (updateLCD) { // every one second
       noInterrupts();
      // get current meter value
       float count = interruptCounter;
@@ -135,10 +151,15 @@ void loop() {
       
       float flowRate = count * FL_a + FL_b;
 
-      Serial.println(flowRate);
+      // Serial.println(flowRate);
+      lcdPrint(current_mA, pressureValue, flowRate);
+      updateLCD = false;
+      if (calibrationCase == 0) {
+        Serial.println(pressureValue);
+      } else if (calibrationCase == 1) {
+        Serial.println(flowRate);
+      }
     }
-
-    // lcdPrint(current_mA, pressureValue, flowRate);
 
   } else {
    if (Serial.available() > 0) {
@@ -146,11 +167,12 @@ void loop() {
      inputString.trim();
      int inputInt = inputString.toInt();
 
-     if (inputInt == 0) {
-       calibrationCase = 0;
-     } else if (inputInt == 1) {
-       calibrationCase = 1;
-     }
+    //  if (inputInt == 0) {
+    //    calibrationCase = 0;
+    //  } else if (inputInt == 1) {
+    //    calibrationCase = 1;
+    //  }
+    ledcWrite(PWM_CHANNEL, inputInt);
    }
   }
 }
